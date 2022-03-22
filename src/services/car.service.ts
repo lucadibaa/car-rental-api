974f543b4b7555af096461c8d593a972ab0e5fa4
Hassam Sohail <hassamsohail709@gmail.com>
2022-03-22 05:18:24 +0300

    service handler for cars

---
+++ src/services/car.service.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const { car } = new PrismaClient();
+
+export const getAllCars = async () => {
+  try {
+    const cars = await car.findMany();
+    return cars;
+  } catch (err: any) {
+    throw new Error(err);
+  }
+};
