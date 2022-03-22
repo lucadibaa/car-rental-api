68b1d994c287d9f371942c6005205e8f06237469
Hassam Sohail <hassamsohail709@gmail.com>
2022-03-22 05:18:42 +0300

    controller for getAllcars

---
+++ src/controllers/car.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { getAllCars } from "../services/car.service";
+
+export const getAllCarsHandler = async (req: Request, res: Response) => {
+  const cars = await getAllCars();
+  if (!cars) return res.sendStatus(404);
+  return res.status(200).json({ success: true, cars });
+};
