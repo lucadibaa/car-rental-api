f94b6fe80b8f7d9d6ad283bc7d1fa4d90bc1c6b6
Hassam Sohail <hassamsohail709@gmail.com>
2022-03-23 04:45:59 +0300

    spaces i guess

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from 'express'
-import 'dotenv/config'
-import deserializeUser from './middlewares/deserializeUser'
-import routes from './routes'
+import express from "express";
+import "dotenv/config";
+import deserializeUser from "./middlewares/deserializeUser";
+import routes from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(deserializeUser)
-app.use(express.json())
+app.use(deserializeUser);
+app.use(express.json());
 
 app.listen(5000, () => {
-    console.log(`App is up and running at http://localhost:${5000}`)
-    routes(app)
-})
+  console.log(`App is up and running at http://localhost:${5000}`);
+  routes(app);
+});
--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,28 +1,31 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const jwt_secret = process.env.JWT_SECRET
+const jwt_secret = process.env.JWT_SECRET;
 
-export const signJwt = (object: Object, options?: jwt.SignOptions | undefined) => {
-    if (!jwt_secret) throw new Error('jwt secret not found')
-    return jwt.sign(object, jwt_secret, options)
-}
+export const signJwt = (
+  object: Object,
+  options?: jwt.SignOptions | undefined
+) => {
+  if (!jwt_secret) throw new Error("jwt secret not found");
+  return jwt.sign(object, jwt_secret, options);
+};
 
 export const verifyJwt = (token: string) => {
-    try {
-        if (!jwt_secret) throw new Error('jwt secret not found')
-        const decoded = jwt.verify(token, jwt_secret)
+  try {
+    if (!jwt_secret) throw new Error("jwt secret not found");
+    const decoded = jwt.verify(token, jwt_secret);
 
-        return {
-            valid: true,
-            expired: false,
-            decoded
-        }
-    } catch (err: any) {
-        console.log(err)
-        return {
-            valid: false,
-            expired: err.message === 'jwt expired',
-            decoded: null
-        }
-    }
-}
+    return {
+      valid: true,
+      expired: false,
+      decoded,
+    };
+  } catch (err: any) {
+    console.log(err);
+    return {
+      valid: false,
+      expired: err.message === "jwt expired",
+      decoded: null,
+    };
+  }
+};
